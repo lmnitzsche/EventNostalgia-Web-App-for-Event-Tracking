e05f2688cf89345bcb42bc2ec7554bdd8d138ab7
lmnitzsche <logannitzsche1@gmail.com>
2025-02-05 13:44:30 -0600

    Revert "working"
    This reverts commit aec307f2c99d2cb3debd078b397aa044027213f7.

--- index.php
+++ index.php
@@ -16,8 +16,8 @@ if(isset($_SESSION["user_id"])) {
 <head>
     <title>Home</title>
     <meta charset="UTF-8">
-    <link rel="icon" href="images/logo.png" type="image/png">
-    <link rel="apple-touch-icon" href="images/logo.png">
+    <link rel="icon" href="images/lmn.png" type="image/png">
+    <link rel="apple-touch-icon" href="images/lmn.png">
     <link rel="stylesheet" href="main/styles.css">
     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4880288893316569"
      crossorigin="anonymous"></script>
